7cfef66c603bd93a28e808669579eb3d1b1c0c12
Adesanya Daniel <4x3l3r8@gmail.com>
2022-09-06 20:10:01 +0100

    consumed post, feed api

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,15 +1,29 @@
+import { useEffect, useState } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
+import axios from "axios";
 import "./feed.css";
-import { Posts } from "../../dummyData";
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    try {
+      const fetchPosts = async () => {
+        let baseUrl = process.env.REACT_APP_API_URL;
+        return axios.get(baseUrl + "posts/timelineposts/63057db0d66927c3bfb70992");
+      };
+
+      fetchPosts().then((res) => setPosts(res.data.data));
+    } catch (e) {}
+  }, []);
+
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        {Posts.map((p) => (
-          <Post key={p.id} post={p} />
+        {posts.map((p) => (
+          <Post key={p._id} post={p} />
         ))}
       </div>
     </div>
--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,35 +1,50 @@
 import "./post.css";
 import { MdMoreVert } from "react-icons/md";
-import { Users } from "../../dummyData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import moment from "moment";
 
 const Post = ({ post }) => {
-  const user = Users.filter((u) => u.id === post.userId);
-
-  const [like, setLike] = useState(post.like);
+  const [like, setLike] = useState(post.likes.length);
   const [isLiked, setIsLiked] = useState(false);
+  const [user, setUser] = useState({});
 
   const likeHandler = () => {
     setLike(isLiked ? like - 1 : like + 1);
     setIsLiked(!isLiked);
   };
 
+  useEffect(() => {
+    try {
+      const fetchUsers = async () => {
+        let baseUrl = process.env.REACT_APP_API_URL;
+        return axios.get(baseUrl + `users/${post.userId}`);
+      };
+
+      fetchUsers().then((res) => setUser(res.data.data));
+    } catch (e) {}
+  }, [post.userId]);
+
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img src={user[0].profilePicture} className="postProfileImg" alt="" />
-            <span className="postUsername">{user[0].username}</span>
-            <span className="postDate">{post.date}</span>
+            <img
+              src={user.profilePicture !== "" ? user.profilePicture : `https://via.placeholder.com/150/000000/ffffff?Text=${user.username}`}
+              className="postProfileImg"
+              alt=""
+            />
+            <span className="postUsername">{user.username}</span>
+            <span className="postDate">{moment(post.createdAt).fromNow()}</span>
           </div>
           <div className="postTopRight">
             <MdMoreVert />
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">{post?.desc}</span>
-          <img src={post.photo} className="postImg" alt="" />
+          <span className="postText">{post?.body}</span>
+          <img src={post.img} className="postImg" alt="" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
@@ -38,7 +53,7 @@ const Post = ({ post }) => {
             <span className="postLikeCounter">{like} people like it</span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentText">{post.comment} comments</span>
+            <span className="postCommentText">{like} comments</span>
           </div>
         </div>
       </div>
