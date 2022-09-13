ec70e9e3485fb81c9adf4cf024d75f6f20f9f4ad
Adesanya Daniel <4x3l3r8@gmail.com>
2022-09-13 18:59:13 +0100

    Setup conditional routing and redirects

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,22 @@ import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+import Protected from "./protected";
 
 function App() {
+  const { user } = useContext(AuthContext)
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route element={<Protected user={user} />}>
+          <Route path="/profile/:username" element={<Profile />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Route>
       </Routes>
     </Router>
   );
