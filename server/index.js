1ab1e5b1e11b05355e3ab91b4375679cbeb51d2b
Adesanya Daniel <4x3l3r8@gmail.com>
2022-09-29 23:34:58 +0100

    Added soclet.io and init settings to server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+
+// === required libraries and initialization
+// socket.io init
+const { createServer } = require("http");
+const socketUtils = require("./utils/socketUtils");
+
 const helmet = require("helmet");
 const cors = require("cors")
 const morgan = require("morgan");
@@ -10,7 +16,9 @@ const userRoute = require("./routes/usersRouter");
 const authRoute = require("./routes/authRouter");
 const postRoute = require("./routes/postRouter");
 const uploadRoute = require("./routes/uploadRouter");
-const errorController = require("./controllers/errorController")
+const conversationRouter = require("./routes/conversationRouter");
+const messageRouter = require("./routes/messageRouter");
+const errorController = require("./controllers/errorController");
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
@@ -28,16 +36,34 @@ app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("common"));
 
+// http init
+const httpServer = createServer(app);
+
+// ws init (exported to be used in socket middleware)
+const io = socketUtils.sio(httpServer)
+
+// socket.io attachment to all requests
+app.all("*", (req, res, next) => {
+    req.io = io;
+    next();
+})
+
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/upload", uploadRoute);
+app.use("/api/conversation", conversationRouter);
+app.use("/api/message", messageRouter);
 
 app.use(errorController);
 
 
 
-app.listen(PORT, () => {
+// ws connect
+socketUtils.connection(io)
+
+// server listen
+httpServer.listen(PORT, () => {
     console.log(`Backend server is running on port: ${PORT}`);
 })
