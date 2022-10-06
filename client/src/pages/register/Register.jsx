5efb02a22042cd50732cea03938f1ee588b05f83
Adesanya Daniel <4x3l3r8@gmail.com>
2022-10-06 23:25:47 +0100

    Implemented Auth components

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,6 +1,19 @@
-import { useContext, useEffect, useRef } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { RegisterCall } from "../../apiCalls";
+import {
+  Desc,
+  LeftSplit,
+  LoginButton,
+  LoginError,
+  LoginForm,
+  LoginInput,
+  LoginPage,
+  LoginWrapper,
+  Logo,
+  RegisterButton,
+  RightSplit,
+} from "../../components/AuthComponents";
 import Spinner from "../../components/spinner";
 import { AuthContext } from "../../context/AuthContext";
 import "./register.css";
@@ -8,24 +21,21 @@ import "./register.css";
 const Register = () => {
   const navigate = useNavigate();
 
-  const username = useRef();
-  const email = useRef();
-  const pass1 = useRef();
-  const pass2 = useRef();
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [pass1, setPass1] = useState("");
+  const [pass2, setPass2] = useState("");
 
   const { user, isFetching, error, dispatch } = useContext(AuthContext);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await RegisterCall(
-      { username: username.current.value, email: email.current.value, password: pass1.current.value, passwordConfirm: pass2.current.value },
-      dispatch
-    );
+    await RegisterCall({ username: username, email: email, password: pass1, passwordConfirm: pass2 }, dispatch);
     if (user) {
-      email.current.value = "";
-      username.current.value = "";
-      pass1.current.value = "";
-      pass2.current.value = "";
+      setEmail("");
+      setUsername("");
+      setPass1("");
+      setPass2("");
 
       navigate("/home");
     }
@@ -33,7 +43,6 @@ const Register = () => {
 
   useEffect(() => {
     // dispatch({ type: "RESTING_STATE" });
-    email.current.focus();
     return () => {
       dispatch({ type: "RESTING_STATE" });
     };
@@ -41,30 +50,58 @@ const Register = () => {
   }, []);
 
   return (
-    <div className="login">
-      <div className="loginWrapper">
-        <div className="loginLeft">
-          <h3 className="loginLogo">ChapChap</h3>
-          <span className="loginDesc">Connect with friends and the world around you on ChapChap.</span>
-        </div>
-        <div className="loginRight">
-          <form onSubmit={(e) => handleSubmit(e)} className="registerBox" autoComplete="false">
-            {error && <div className="loginError">{error.Message || error}</div>}
-            <input placeholder="Username" autoComplete={"off"} ref={username} id="username" className="loginInput" />
-            <input placeholder="Email" ref={email} className="loginInput" />
-            <input type="password" ref={pass1} placeholder="Password" id="password" className="loginInput" />
-            <input type="password" ref={pass2} placeholder="Confirm Password" id="passwordConfirm" className="loginInput" />
-            <button className={`loginButton ${isFetching ? "loading" : ""}`} type={"submit"} disabled={isFetching}>
+    <LoginPage>
+      <LoginWrapper>
+        <LeftSplit>
+          <Logo>ChapChap</Logo>
+          <Desc>Connect with friends and the world around you on ChapChap.</Desc>
+        </LeftSplit>
+        <RightSplit>
+          <LoginForm onSubmit={(e) => handleSubmit(e)} autoComplete="false">
+            {error && <LoginError>{error.Message || error}</LoginError>}
+            <LoginInput
+              type="email"
+              placeholder="Email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              autoComplete={"off"}
+              id="username"
+            />
+            <LoginInput
+              type="text"
+              placeholder="Username"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+            <LoginInput
+              type="password"
+              placeholder="Password"
+              onChange={(e) => {
+                setPass1(e.target.value);
+              }}
+              id="password"
+            />
+            <LoginInput
+              type="password"
+              placeholder="Confirm Password"
+              onChange={(e) => {
+                setPass2(e.target.value);
+              }}
+              id="passwordConfirm"
+            />
+            <LoginButton isFetching={isFetching} type={"submit"} disabled={isFetching}>
               {isFetching ? <Spinner /> : "Register"}
-            </button>
+            </LoginButton>
             {/* <a href="/" className="loginForgot">Forgot Password</a> */}
-            <button onClick={() => navigate("/login")} disabled={isFetching} className="loginRegisterButton">
+            <RegisterButton onClick={() => navigate("/login")} disabled={isFetching}>
               Log in to your account
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
+            </RegisterButton>
+          </LoginForm>
+        </RightSplit>
+      </LoginWrapper>
+    </LoginPage>
   );
 };
 
